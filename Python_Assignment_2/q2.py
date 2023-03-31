c42226439ad68e351625a74d29fc52309289ed56
Rohan <129480723+Rohanjaintb@users.noreply.github.com>
2023-03-31 15:48:55 +0530

    Delete Python_Assignment_2 directory

--- Python_Assignment_2/q2.py
+++
@@ -1,9 +0,0 @@
-n=str(input("Enter the Name:"))#user input
-m=n.split(" ")# user list created
-
-for j in m:
-    vowels = "aeiouAEIOU"
-   
-    for vowel in vowels:
-        j = j.replace(vowel, "")#replace a vowels by space
-    print(j,end=" ")
--- Python_Assignment_2/q3.py
+++
@@ -1,5 +0,0 @@
-n=str(input("Enter the Name:"))#user input
-m=n.split(" ")# user list created
-print(m)
-k=sorted(m)#sort value according alphabetical order
-print(k)
