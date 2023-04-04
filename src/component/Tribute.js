1fb5e3c78ac20aec3d502cde6aa8a50e35601ba9
Rohan <129480723+Rohanjaintb@users.noreply.github.com>
2023-04-04 15:13:28 +0530

    Add files via upload

---
+++ src/component/Tribute.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./styles.css";
+
+function TributePage() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Jedeja</h1>
+        <img src="https://img.etimg.com/thumb/width-1200,height-900,imgsize-88278,resizemode-1,msid-90020119/news/sports/rockstar-ravindra-jadeja-on-day-2-slams-175-to-put-india-in-command-in-first-test-against-sri-lanka.jpg" alt="" />
+      </header>
+      <section>
+        <h2>About Jedeja</h2>
+        <p>
+          Jedeja is a visionary entrepreneur who founded Jedeja Inc. in 2010.
+          Under his leadership, Jedeja Inc. has become one of the most successful
+          companies in the tech industry.
+        </p>
+        <p>
+          Jedeja is known for his innovative ideas and his ability to turn them
+          into reality. He has a strong passion for technology and is always
+          looking for ways to improve the lives of people through his products.
+        </p>
+      </section>
+      <section>
+        <h2>Accomplishments</h2>
+        <ul>
+          <li>Founded Jedeja Inc. in 2010</li>
+          <li>Developed the popular Jedeja software suite</li>
+          <li>Received the Tech Innovator of the Year award in 2015</li>
+          <li>Named one of the Top Entrepreneurs Under 40 by Forbes in 2017</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Legacy</h2>
+        <p>
+          Jedeja's legacy will live on through the many products and services
+          developed by Jedeja Inc. His dedication to innovation and improving
+          the lives of people through technology has inspired a new generation
+          of entrepreneurs to follow in his footsteps.
+        </p>
+      </section>
+      <footer>
+        <p>
+          This tribute page was created by [Your Name]. You can find the source
+          code on [GitHub/CodePen/Your preferred platform].
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default TributePage;
